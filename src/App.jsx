62d6bb4782fce084ec61bd5c1286c86015a564d4
aminulhasan142 <mahshoukhin142@gmail.com>
2025-09-24 05:51:21 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <div className="bg-blue-950">
+      <Navbar />
+      <h1 className="text-2xl">hellonpm</h1>
+    </div>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,3 @@
+export default function Navbar() {
+  return <h1 className="text-green-600">navbar</h1>;
+}
