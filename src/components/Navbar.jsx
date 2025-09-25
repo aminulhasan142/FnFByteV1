647773f9d85d01c7db71883133f67afee9dc222f
aminulhasan142 <mahshoukhin142@gmail.com>
2025-09-26 00:25:59 +0600

    nav working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,23 @@
 export default function Navbar() {
-  return <h1 className="text-green-600">navbar</h1>;
+  return (
+    <div className="w-full flex justify-center items-center h-[4.5rem] py-3 fixed backdrop-blur-md bg-navbar">
+      <div className="flex max-w-[1440px] justify-between items-center self-stretch">
+        {/* logo */}
+        <a href="#" className="flex items-center">
+          <img src="/src/assets/logo.svg" alt="logo" />
+          <div className="flex justify-center items-center px-4 py-3 rounded-2xl">
+            <span className="text-white font-poppins text-lg font-bold leading-[1.8rem]">
+              FnF
+            </span>
+            <span className="text-indigo-500 font-poppins text-lg font-bold leading-[1.8rem]">
+              Byte
+            </span>
+          </div>
+        </a>
+
+        {/* menu */}
+        <div></div>
+      </div>
+    </div>
+  );
 }
