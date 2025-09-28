26e7477aa2dc891ddbf226d6754432ba864c0522
aminulhasan142 <mahshoukhin142@gmail.com>
2025-09-28 23:18:26 +0600

    work done on service gig

--- src/assets/PopularTag.jsx
+++ src/assets/PopularTag.jsx
@@ -1,11 +1,14 @@
+import { MdStars } from "react-icons/md";
+
 export default function PopularTag() {
   return (
     <span className="flex py-2 pl-2 pr-4 justify-center gap-1 rounded-full border border-border bg-[#ffffff0d]">
-      <img
+      {/* <img
         src="/src/assets/stars.svg"
         alt="star"
         className="w-5 h-5 lg:w-4 lg:h-4"
-      />
+      /> */}
+      <MdStars className="text-[#fb923c]" />
       <p className="text-white text-sm font-poppins font-normal"> Popular</p>
     </span>
   );
--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -55,7 +55,7 @@ export default function ServicesSection() {
       {/* 2nd box container */}
       <div className="flex flex-col justify-center items-start gap-1 w-full lg:w-[40%]">
         {/* gig card 1 */}
-        <div className="flex flex-col pt-6 md:pt-12 lg:pt-[8.5rem] px-6 md:px-12 lg:px-16 pb-6 md:pb-6 lg:pb-16 items-start gap-5 self-stretch rounded-3xl bg-linear-[180deg] from-[#dbdde00a] from-0% to-[#0049f50a] to-100%">
+        <div className="flex flex-col pt-6 md:pt-12 lg:pt-[8.5rem] px-4 md:px-12 lg:px-16 pb-6 md:pb-6 lg:pb-16 items-start gap-5 self-stretch rounded-3xl bg-linear-[180deg] from-container-gray1 from-0% to-container-gray2 to-100%">
           <img src="/src/assets/profile.svg" alt="Profile" />
           {/* gig description */}
           <div className="flex flex-col items-start gap-6 self-stretch">
@@ -128,11 +128,143 @@ export default function ServicesSection() {
                 </p>
                 <PopularTag />
               </div>
-              <div className="w-full py-3 border-b border-[#ffffff0d] flex justify-between">
+              <div className="w-full py-3 border-[#ffffff0d] flex justify-between">
                 <p className="text-xl lg:text-2xl font-semibold text-white ">
                   AI Product Design
                 </p>
                 <PopularTag />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* gig card 2 */}
+        <div className="flex flex-col pt-6 md:pt-12 lg:pt-16 px-4 md:px-12 lg:px-16 pb-6 md:pb-6 lg:pb-16 items-start gap-5 self-stretch rounded-3xl bg-linear-[180deg] from-container-gray2 from-0% to-container-gray2 to-100%">
+          <img src="/src/assets/CodeIcon.svg" alt="Profile" />
+          {/* gig description */}
+          <div className="flex flex-col items-start gap-6 self-stretch">
+            {/* title heading */}
+            <div className="flex flex-col items-start gap-3 self-stretch">
+              <div className="flex justify-between items-center gap-1 self-stretch">
+                <h1 className="text-4xl font-black uppercase bg-linear-[180deg] from-[#ffffff1a] from-0% to-white to-100% bg-clip-text text-transparent">
+                  DEVELOPMENT
+                </h1>
+                {/* button */}
+                <div className={`flex items-center justify-center`}>
+                  <a
+                    href="#link"
+                    className={`moving_arrow_button p-3 rounded-full transition ease-out duration-500  text-white font-First font-bold gap-1 text-sm self-center justify-center items-center flex  bg-slate-800`}
+                  >
+                    <div className="flex flex-col h-5 w-6 relative overflow-hidden">
+                      <span className="material-symbols-outlined relative  transition ease-out duration-200">
+                        arrow_outward
+                      </span>
+                      <span className="material-symbols-outlined relative -left-6  transition ease-out duration-200">
+                        arrow_outward
+                      </span>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              {/* sub title text */}
+              <p className="font-poppins text-slate-400 text-sm font-medium">
+                Building scalable, secure, and high-performance digital
+                products.
+              </p>
+            </div>
+            {/* gig title heading */}
+            <div className="flex flex-col items-start gap-5 self-stretch">
+              <div className="w-full py-3 border-b border-[#ffffff0d] flex justify-between">
+                <p className="text-xl lg:text-2xl font-semibold text-white ">
+                  Mobile App Development
+                </p>
+              </div>
+              <div className="w-full py-3 border-b border-[#ffffff0d] flex justify-between">
+                <p className="text-xl lg:text-2xl font-semibold text-white ">
+                  Web Development
+                </p>
+              </div>
+              <div className="w-full py-3 border-b border-[#ffffff0d] flex justify-between">
+                <p className="text-xl lg:text-2xl font-semibold text-white ">
+                  Shopify Development
+                </p>
+              </div>
+              <div className="w-full py-3 border-b border-[#ffffff0d] flex justify-between">
+                <p className="text-xl lg:text-2xl font-semibold text-white ">
+                  Wordpress Development
+                </p>
+                <PopularTag />
+              </div>
+              <div className="w-full py-3 border-b border-[#ffffff0d] flex justify-between">
+                <p className="text-xl lg:text-2xl font-semibold text-white ">
+                  MVPs & SaaS Platforms
+                </p>
+                <PopularTag />
+              </div>
+              <div className="w-full py-3  border-[#ffffff0d] flex justify-between">
+                <p className="text-xl lg:text-2xl font-semibold text-white ">
+                  Dashboards & Landing Pages
+                </p>
+                <PopularTag />
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* gig card 3 */}
+        <div className="flex flex-col pt-6 md:pt-12 lg:pt-16 px-4 md:px-12 lg:px-16 pb-6 md:pb-6 lg:pb-16 items-start gap-5 self-stretch rounded-3xl bg-linear-[180deg] from-container-gray2 from-0% to-container-gray1 to-100%">
+          <img src="/src/assets/AI&MLicon.svg" alt="Profile" />
+          {/* gig description */}
+          <div className="flex flex-col items-start gap-6 self-stretch">
+            {/* title heading */}
+            <div className="flex flex-col items-start gap-3 self-stretch">
+              <div className="flex justify-between items-center gap-1 self-stretch">
+                <h1 className="text-4xl font-black uppercase bg-linear-[180deg] from-[#ffffff1a] from-0% to-white to-100% bg-clip-text text-transparent">
+                  AI & ML Solutions
+                </h1>
+                {/* button */}
+                <div className={`flex items-center justify-center`}>
+                  <a
+                    href="#link"
+                    className={`moving_arrow_button p-3 rounded-full transition ease-out duration-500  text-white font-First font-bold gap-1 text-sm self-center justify-center items-center flex  bg-slate-800`}
+                  >
+                    <div className="flex flex-col h-5 w-6 relative overflow-hidden">
+                      <span className="material-symbols-outlined relative  transition ease-out duration-200">
+                        arrow_outward
+                      </span>
+                      <span className="material-symbols-outlined relative -left-6  transition ease-out duration-200">
+                        arrow_outward
+                      </span>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              {/* sub title text */}
+              <p className="font-poppins text-slate-400 text-sm font-medium">
+                Unlocking business growth with intelligent automation and
+                analytics.
+              </p>
+            </div>
+            {/* gig title heading */}
+            <div className="flex flex-col items-start gap-5 self-stretch">
+              <div className="w-full py-3 border-b border-[#ffffff0d] flex justify-between">
+                <p className="text-xl lg:text-2xl font-semibold text-white ">
+                  Predictive Analytics
+                </p>
+              </div>
+              <div className="w-full py-3 border-b border-[#ffffff0d] flex justify-between">
+                <p className="text-xl lg:text-2xl font-semibold text-white ">
+                  AI-Powered Chatbots
+                </p>
+                <PopularTag />
+              </div>
+              <div className="w-full py-3 border-b border-[#ffffff0d] flex justify-between">
+                <p className="text-xl lg:text-2xl font-semibold text-white ">
+                  Data-Driven Dashboards
+                </p>
+              </div>
+              <div className="w-full py-3  border-[#ffffff0d] flex justify-between">
+                <p className="text-xl lg:text-2xl font-semibold text-white ">
+                  ML Tools & Automation
+                </p>
               </div>
             </div>
           </div>
